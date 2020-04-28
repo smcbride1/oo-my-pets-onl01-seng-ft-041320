3c5c3c2bd41cffe59fbfec41d512a36208567f99
Stephen McBride <stevemcbride3@gmail.com>
2020-04-28 19:13:06 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,26 @@
 class Dog
-  # code goes here
+  attr_accessor :mood
+  attr_reader :name, :owner
+
+  @@all = []
+
+  def initialize(name, new_owner)
+    @name = name
+    self.owner = new_owner #THE .SELF IN FRONT OF THE OWNER METHOD IS SUPER IMPORTANT!!! OWNER METHOD ISN'T CALLED ON INSTANCE IF SELF ISN'T THERE
+    @mood = "nervous"
+    @@all.push(self)
+  end
+
+  def self.all
+    @@all
+  end
+
+  def owner=(new_owner)
+    owner.pets.delete(self) if owner != nil
+    owner.dogs.delete(self) if owner != nil
+    @owner = new_owner
+    return if owner == nil
+    owner.pets.push(self)
+    owner.dogs.push(self)
+  end
 end
--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,70 @@
 class Owner
-  # code goes here
+  attr_accessor :dogs, :cats, :pets
+  attr_reader :name, :species
+
+  @@all = []
+
+  def initialize(name)
+    @pets = []
+    @dogs = []
+    @cats = []
+    @name = name
+    @species = "human"
+    @@all.push(self)
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.count
+    @@all.length
+  end
+
+  def self.reset_all
+    @@all.clear
+  end
+
+  def say_species
+    "I am a #{species}."
+  end
+
+  def buy_cat(name)
+    Cat.new(name, self)
+  end
+
+  def buy_dog(name)
+    Dog.new(name, self)
+  end
+
+  def sell_pets
+    sell_cats
+    sell_dogs
+  end
+
+  def sell_cats
+    cats.each do |cat|
+      cat.mood = "nervous"
+      cat.owner = nil
+    end
+  end
+
+  def sell_dogs
+    dogs.each do |dog|
+      dog.mood = "nervous"
+      dog.owner = nil
+    end
+  end
+
+  def walk_dogs
+    dogs.each {|dog| dog.mood = "happy"}
+  end
+
+  def feed_cats
+    cats.each {|cat| cat.mood = "happy"}
+  end
+
+  def list_pets
+    "I have #{dogs.length} dog(s), and #{cats.length} cat(s)."
+  end
 end
